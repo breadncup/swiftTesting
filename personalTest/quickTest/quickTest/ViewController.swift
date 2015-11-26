9e37911917cf571f5e9ab60a9e0f22a6ccae53df
Daniel (Youngwhan) Song <breadncup@gmail.com>
2015-11-26 00:23:42 -0800

    Updated test code

--- personalTest/quickTest/quickTest/ViewController.swift
+++ personalTest/quickTest/quickTest/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func test1() {
+        print("0xC.3p0: \(0xC.3p0)")
+        print("0xC.3p1: \(0xC.3p1)")
+        print("0xC.3p2: \(0xC.3p2)")
+        print("0xC.3p2: \(0xC.3p3)")
+        print("0xC.3p2: \(0xC.3p4)")
+        print("0xC.31p2: \(0xC.31p0)")
+        print("0xC.314p2: \(0xC.314p0)")
+        print("0xC.314p2: \(0xC.314p2)")
+        print("1_000_000.000_000_12: \(1_000_000.000_000_12)")
+        
+        let age = -3
+        assert( age >= 0, "A person age cannot be negative")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
